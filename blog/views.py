2ef6c2908753a939e4b035a02782fb6a5bc1e4ea
g14s3r <g14s3r@gmail.com>
2020-02-17 20:46:03 +0300

    Added 'pagination'. Changed colour theme

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
